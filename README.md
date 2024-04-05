0c4c3daafb3734960349c95ef34b46abb760289e
Edward Scott Johnson <edwardsj13579@gmail.com>
2024-04-05 11:51:41 -0300

    Chatbot skeleton

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+# Spontaneous Chatbot
+A chatbot that interacts with the user without direct input. It acts on its own volition by reading the messages being sent and deciding if it wants to interact futher with the user. Other variables affect its thought process, such as the bots current mood, what its thinking and what its curious about.
+
 In development
--- botCLI.py
+++ botCLI.py
@@ -1,12 +1,23 @@
 import lmql
+import LMQLsrc
 import time
 
+
+# Load in models
 path = "/home/pressprexx/Code/Models/"
-model = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
+modelPath = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
+lightModelPath = ""
 
-model_file = path + model
+lightModelFile = path + lightModelPath
+modelFile = path + modelPath
 
-model = lmql.model(f"local:llama.cpp:{model_file}", 
+model = lmql.model(f"local:llama.ccp:{modelFile}",
+    cuda = True,
+    n_ctx = 2048,
+    verbose = False
+)
+
+lightModel = lmql.model(f"local:llama.cpp:{lightModelFile}", 
     tokenizer = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
     cuda = True,
     n_ctx = 2048,
@@ -14,48 +25,18 @@ model = lmql.model(f"local:llama.cpp:{model_file}",
     # verbose=True
 )
 
-@lmql.query(model=model, is_async=False, max_len = 100)
-def isOffensive(message):
-    '''lmql
-    "Is '{message}' offensive or contains inappropriate words? Answer simply 'yes' or 'no':"
-    "Is '{message}' offensive?: [OFFENSIVE]" where OFFENSIVE in ["yes", "no"]
-    if OFFENSIVE == "yes":
-        OFFENSIVE = True
-        "Generate a censored version of '{message}' changing some letters for symbols:"
-        "Censored version: [CENSORED]" where len(TOKENS(CENSORED)) < 10
-    else:
-        OFFENSIVE = False
-        CENSORED = message
-    return OFFENSIVE
-    '''
 
-@lmql.query(model=model, is_async=False, max_len = 100)
-def isQuestion(message):
-    '''lmql
-    """
-    Is the message below a question? 
-    {message}.
-    Answer simply 'yes' or 'no': [QUESTION]
-    """ where QUESTION in ["yes", "no"]
-    if QUESTION == "yes":
-      QUESTION = True
-    else:
-      QUESTION = False
-    return QUESTION
-    '''
+# Create variables
+messages = {}
+userName = ""
+lastMessages = {}
+lastMessage = {}
+lastUserMessage = ""
+lastBotMessage = ""
+shortSummary = ""
 
-@lmql.query(model=model, is_async=False, max_len = 100)
-def isAskedToAbort(message):
-    '''lmql
-    "Does '{message}' explicitly indicate that the user wants the conversation to end? Answer simply 'yes' or 'no':"
-    "Does the user want to end the conversation?: [ABORT]" where ABORT in ["yes", "no"]
-    if ABORT == "yes":
-        ABORT = True
-    else:
-        ABORT = False
-    return ABORT
-    '''
 
+# Helpful functions
 def testPrompt(promptName, message):
     start = time.time()
     output = promptName(message)  # Assuming lmql handles this synchronously
@@ -64,31 +45,15 @@ def testPrompt(promptName, message):
     print(output)
 
 
-
+# Chatbot
 def main():
-    # start = time.time()
-    # offensive = isOffensive("fuck off")  # Assuming lmql handles this synchronously
-    # result = time.time() - start
-    # print(f'EvalOffensive: {result:.2f}')
-    # print(offensive)
 
-    testPrompt(isOffensive, "fuck off")
+    testPrompt(LMQLsrc.isOffensive, "Fuck off")
     print("\n")
-    # start = time.time()
-    # question = isQuestion("You are great!")  # Assuming lmql handles this synchronously
-    # result = time.time() - start
-    # print(f'EvalQuestion: {result:.2f}')
-    # print(question)
-
-    testPrompt(isQuestion, "You are great!")
+    testPrompt(LMQLsrc.isQuestion, "You are great!")
     print("\n")
-    testPrompt(isAskedToAbort, "I don't want to talk anymore")
+    testPrompt(LMQLsrc.isAskedToAbort, "I don't want to talk anymore")
     print("\n")
-    # start = time.time()
-    # abort = isAskedToAbort("I don't want to talk anymore")  # Assuming lmql handles this synchronously
-    # result = time.time() - start
-    # print(f'EvalAbort: {result:.2f}')
-    # print(abort)
 
 if __name__ == "__main__":
     main()
