50f2b37eb392a21215be6a49b15c9fdb8687c1a4
Edward <edwardsj1379@gmail.com>
2024-04-05 00:34:51 -0300

    prompt testing

--- botCLI.py
+++ botCLI.py
@@ -1,3 +1,20 @@
+import lmql
+import time
+
+path = "/home/pressprexx/Code/Models/"
+model = "tinyllama-1.1b-chat-v1.0.Q6_K.gguf"
+
+model_file = path + model
+
+model = lmql.model(f"local:llama.cpp:{model_file}", 
+    tokenizer = "TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    cuda = True,
+    n_ctx = 2048,
+    verbose = False
+    # verbose=True
+)
+
+@lmql.query(model=model, is_async=False, max_len = 100)
 def isOffensive(message):
     '''lmql
     "Is '{message}' offensive or contains inappropriate words? Answer simply 'yes' or 'no':"
@@ -5,14 +22,15 @@ def isOffensive(message):
     if OFFENSIVE == "yes":
         OFFENSIVE = True
         "Generate a censored version of '{message}' changing some letters for symbols:"
-        "Censored version: [CENSORED]"
+        "Censored version: [CENSORED]" where len(TOKENS(CENSORED)) < 10
     else:
         OFFENSIVE = False
         CENSORED = message
     return OFFENSIVE
     '''
 
-def evalIsAQuestion(message):
+@lmql.query(model=model, is_async=False, max_len = 100)
+def isQuestion(message):
     '''lmql
     """
     Is the message below a question? 
@@ -26,15 +44,51 @@ def evalIsAQuestion(message):
     return QUESTION
     '''
 
-
-def evalIsAskedToAbort(message):
+@lmql.query(model=model, is_async=False, max_len = 100)
+def isAskedToAbort(message):
     '''lmql
-    "Does '{message}' explicitly indicates that the user wants the conversation to end? Answer simply 'yes' or 'no':"
-    "The user wants to end the conversation?: [ABORT]" where ABORT in ["yes", "no"]
-    ABORT = getMostSimilarString(ABORT, ["yes", "no"])
+    "Does '{message}' explicitly indicate that the user wants the conversation to end? Answer simply 'yes' or 'no':"
+    "Does the user want to end the conversation?: [ABORT]" where ABORT in ["yes", "no"]
     if ABORT == "yes":
         ABORT = True
     else:
         ABORT = False
     return ABORT
     '''
+
+def testPrompt(promptName, message):
+    start = time.time()
+    output = promptName(message)  # Assuming lmql handles this synchronously
+    result = time.time() - start
+    print(f'Time of {promptName.__name__}: {result:.2f}')
+    print(output)
+
+
+
+def main():
+    # start = time.time()
+    # offensive = isOffensive("fuck off")  # Assuming lmql handles this synchronously
+    # result = time.time() - start
+    # print(f'EvalOffensive: {result:.2f}')
+    # print(offensive)
+
+    testPrompt(isOffensive, "fuck off")
+    print("\n")
+    # start = time.time()
+    # question = isQuestion("You are great!")  # Assuming lmql handles this synchronously
+    # result = time.time() - start
+    # print(f'EvalQuestion: {result:.2f}')
+    # print(question)
+
+    testPrompt(isQuestion, "You are great!")
+    print("\n")
+    testPrompt(isAskedToAbort, "I don't want to talk anymore")
+    print("\n")
+    # start = time.time()
+    # abort = isAskedToAbort("I don't want to talk anymore")  # Assuming lmql handles this synchronously
+    # result = time.time() - start
+    # print(f'EvalAbort: {result:.2f}')
+    # print(abort)
+
+if __name__ == "__main__":
+    main()
